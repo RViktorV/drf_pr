ba1da0001a2d50ec4efd26545de2413652bd1af9
RViktorV <rgavih@yandex.ru>
2024-09-13 10:48:16 +0700

    13.09.2024 12:50

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from learning.views import CourseViewSet  # Импортируем ViewSet из приложения learning
-from users.views import UsersViewSet  # Импортируем ViewSet для пользователей
 
-router = DefaultRouter()
-router.register(r"courses", CourseViewSet)  # Регистрируем ViewSet для курсов
-router.register(r"users", UsersViewSet)  # Регистрируем ViewSet для пользователей
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "api/", include(router.urls)
-    ),  # Подключаем маршруты API для курсов и пользователей
-    path("api/", include("learning.urls")),  # Подключаем отдельные маршруты для уроков
+    path("", include("learning.urls")),  # Подключаем отдельные маршруты для уроков
+    path("", include("users.urls")),  # Подключаем отдельные маршруты для пользователей
 ]
--- learning/urls.py
+++ learning/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from .views import LessonListCreateView, LessonRetrieveUpdateDestroyView
+from rest_framework.routers import DefaultRouter
+from learning.views import CourseViewSet  # Импортируем ViewSet из приложения learning
+
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet)  # Регистрируем ViewSet для курсов
 
 urlpatterns = [
     path("lessons/", LessonListCreateView.as_view(), name="lesson-list-create"),
-    path(
-        "lessons/<int:pk>/",
-        LessonRetrieveUpdateDestroyView.as_view(),
-        name="lesson-detail",
-    ),
+    path("lessons/<int:pk>/",
+         LessonRetrieveUpdateDestroyView.as_view(),
+         name="lesson-detail",
+         ),
+
+    path("", include(router.urls)),  # Подключаем маршруты API для курсов и пользователей
 ]
