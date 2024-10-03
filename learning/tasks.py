12fca2ff0ee79bb3a45ba256cdb4085ea0cb6dd0
vitek <rgavih@yandex.ru>
2024-10-03 21:45:30 +0700

    dz_26_2 03/10/24 21:45

--- learning/tasks.py
+++ learning/tasks.py
@@ -1,10 +1,24 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
+from .models import Course, Subscription
+
 
 @shared_task
-def send_course_update_email(course_title, user_email):
-    subject = f"Обновление курса: {course_title}"
-    message = f"Курс {course_title} был обновлен. Проверьте новые материалы."
-    email_from = settings.DEFAULT_FROM_EMAIL
-    send_mail(subject, message, email_from, [user_email])
+def send_course_update_email_async(course_id):
+    """
+    Асинхронная задача для отправки уведомлений о обновлении курса подписчикам.
+
+    Args:
+        course_id (int): ID курса, который был обновлен.
+    """
+    course = Course.objects.get(id=course_id)
+    subscribers = Subscription.objects.filter(course=course)
+
+    for subscriber in subscribers:
+        send_mail(
+            subject=f'Обновление курса: {course.title}',
+            message=f'Курс "{course.title}" был обновлен.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[subscriber.user.email],
+        )
